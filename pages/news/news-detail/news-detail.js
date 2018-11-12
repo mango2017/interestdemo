cdc6fa0a9f37a01d984782ff77248c5da814b838
mango <1210056515@qq.com>
2018-11-12 09:38:43 +0800

    新闻详情页面

--- pages/news/news-detail/news-detail.js
+++ pages/news/news-detail/news-detail.js
@@ -6,16 +6,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-    detailData:[]
+    // detailData:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var detailData = newsData.initData[options.newsid];
-    this.setData({
-      detailData: detailData
-    })
+    // var detailData = newsData.initData[options.newsid];
+    // console.log(detailData);
+    this.setData(newsData.initData[options.newsid])
   },
 })
