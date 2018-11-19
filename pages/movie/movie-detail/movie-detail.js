8ae65ce99bd07d3522897a3dc76468c6bee91ae8
mango <1210056515@qq.com>
2018-11-19 15:10:39 +0800

    电影详情页面

--- pages/movie/movie-detail/movie-detail.js
+++ pages/movie/movie-detail/movie-detail.js
@@ -1,67 +1,76 @@
 // pages/movie/movie-detail/movie-detail.js
+var app = getApp();
+var utils = require("../../util/util.js");
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    movie: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    console.log(options.movieid);
+  onLoad: function(options) {
+    //页面初始化，options为页面跳转所带来的参数
+    var movieId = options.movieid;
+    //url地址
+    var detailMovieUrl = app.globalUrl.doubanUrl + "/v2/movie/subject/" + movieId;
+    //console.log(detailMovieUrl);
+    utils.http(detailMovieUrl, this.callback);
 
   },
+  callback: function(data) {
+    if (!data) {
+      return;
+    }
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    //处理一下导演
+    var director = {
+      avatar: "",
+      name: "",
+      id: ""
+    }
 
+    if (data.directors[0] != null) {
+      if (data.directors[0].avatarts != null) {
+        director.avatar = data.directors[0].avatars.large;
+      }
+      director.name = data.directors[0].name;
+      director.id = data.directors[0].id;
+    }
+
+
+    var temp = {
+      movieImg: data.images.large,
+      country: data.countries[0],
+      title: data.title,
+      originalTitle: data.original_title,
+      wishCount: data.wish_count,
+      commentCount: data.comments_count,
+      year: data.year,
+      genres: data.genres,
+      stars: utils.converToStarsArray(data.rating.stars),
+      score: data.rating.average,
+      director: director,
+      casts: utils.convertToCastString(data.casts),
+      castsInfo: utils.convertToCastsString(data.casts),
+      summary: data.summary
+    }
+
+    this.setData({
+      movie: temp
+    })
+    wx.setNavigationBarTitle({
+      title: this.data.movie.title,
+    })
   },
+  // onReady:function(){
+  //   wx.setNavigationBarTitle({
+  //     title: this.data.movie.title,
+  //   })
+  // }
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
--- pages/util/util.js
+++ pages/util/util.js
@@ -27,10 +27,34 @@ function http(url,callback){
       }
     })
 }
+//演员名字使用"/"分隔开
+function convertToCastString(casts){
+  for(var dic in casts){
+    var castsjoin = "";
+    castsjoin = castsjoin + casts[dic].name + " / ";
+  }
+  return castsjoin.substring(0,castsjoin.length-3);
+}
 
-
+//处理演员信息：头像+名字
+function convertToCastsString(casts){
+  //存储信息：头像+名字
+  var castsArray = [];
+  for(var idx in casts){
+    var cast = {
+      img: casts[idx].avatars ? casts[idx].avatars.large:"",
+      name: casts[idx].name
+    }
+    castsArray.push(cast);
+  }
+  // console.log("hello world");
+  // console.log(castsArray)
+  return castsArray;
+}
 
 module.exports = {
   converToStarsArray: converToStarsArray,
-  http:http
+  http:http,
+  convertToCastString: convertToCastString,
+  convertToCastsString: convertToCastsString
 }
