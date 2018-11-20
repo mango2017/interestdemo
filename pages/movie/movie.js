06ef9a4aab035b06682d30ab7ff6fa99b2236960
mango <1210056515@qq.com>
2018-11-20 10:51:32 +0800

    搜索电影

--- pages/movie/movie.js
+++ pages/movie/movie.js
@@ -7,9 +7,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-    "in_theaters":[],
-    "coming_soon":[],
-    "top250":[]
+    in_theaters:[],
+    coming_soon:[],
+    top250:[],
+    containerShow:true,
+    searchPanelShow:false,
+    searchData:[]
+    
   },
 
   /**
@@ -37,34 +41,6 @@ Page({
       }
     })
   },
-
-  callback: function (res, category,categoryName){
-    // console.log(category);
-    var movies = [];
-    for(var idx in res.subjects){
-      var subject =  res.subjects[idx];
-      // console.log(subject);
-      var title = subject.title;
-      if(title.length >= 6){
-        title = title.substring(0,6)+"...";
-      }
-      var temp = {
-        title:title,
-        converageUrl:subject.images.large,
-        star: utils.converToStarsArray(subject.rating.stars),
-        average:subject.rating.average,
-        movieid:subject.id
-      }
-      movies.push(temp);
-    }
-    var readyData = {};
-    readyData[category] = {
-      categoryName: categoryName,
-      movies: movies
-    }
-    // console.log(readyData);
-    this.setData(readyData)
-  },
   movieMoreTap:function(event){
     var categoryName = event.currentTarget.dataset.categoryname;
     wx.navigateTo({
@@ -77,6 +53,56 @@ Page({
     wx.navigateTo({
       url: 'movie-detail/movie-detail?movieid='+movieId,
     })
-  }
-
+  },
+  //触发焦点
+  onBindFocus:function(event){
+    this.setData({
+      containerShow:false,
+      searchPanelShow:true
+    })
+  },
+  //失去焦点
+  onBindBlur:function(event){
+    //网络请求
+    //获取用户输入信息
+    var text = event.detail.value;
+    //确定url
+    var searchUrl = app.globalUrl.doubanUrl+ "/v2/movie/search?q="+text;
+    this.http(searchUrl, this.callback,"searchData","");
+    wx.showNavigationBarLoading();
+  },
+  //取消
+  onCancelImgTap:function(event){
+    this.setData({
+      containerShow: true,
+      searchPanelShow: false
+    })
+  },
+  callback: function (res, category, categoryName) {
+    var movies = [];
+    for (var idx in res.subjects) {
+      var subject = res.subjects[idx];
+      // console.log(subject);
+      var title = subject.title;
+      if (title.length >= 6) {
+        title = title.substring(0, 6) + "...";
+      }
+      var temp = {
+        title: title,
+        converageUrl: subject.images.large,
+        star: utils.converToStarsArray(subject.rating.stars),
+        average: subject.rating.average,
+        movieid: subject.id
+      }
+      movies.push(temp);
+    }
+    var readyData = {};
+    readyData[category] = {
+      categoryName: categoryName,
+      movies: movies
+    }
+    // console.log(readyData);
+    this.setData(readyData);
+    wx.hideNavigationBarLoading();
+  },
 })
